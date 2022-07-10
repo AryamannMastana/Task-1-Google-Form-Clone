4dc72f35e00f428b5dfdae8415429e30bf72ac6f
Aryamann <aryamannmastana@gmail.com>
2022-07-10 23:57:55 +0530

    HTML, CSS and JS files added

---
+++ script.js
@@ -0,0 +1,14 @@
+var nameInput = document.getElementById('name');
+var collegeInput = document.getElementById('college');
+var emailInput = document.getElementById('email');
+var mobileInput = document.getElementById('mobile');
+
+// document.querySelector('div.form').addEventListener('submit', function(e){
+//     e.preventDefault;
+// });
+
+console.log(nameInput.value);
+console.log(collegeInput.value);
+console.log(emailInput.value);
+console.log(mobileInput.value);
+
